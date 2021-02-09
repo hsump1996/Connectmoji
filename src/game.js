d3724116b6e95048cd67ee95a052f6c1e0b80b6f
Sunpyo Hong <sh4023@nyu.edu>
2021-02-10 06:07:36 +0900

    Update game.js

--- src/game.js
+++ src/game.js
@@ -14,69 +14,83 @@ const useScriptedMoves = function () {
   const board = c.generateBoard(numRows, numColumns);
 
   let stateAfterAutoPlay = c.autoplay(board, playersAndMoveString, numConsecutive);
-  let letter = null;
-
   readlineSync.question('Press <ENTER> to continue...');
-  console.log(c.boardToString(stateAfterAutoPlay.board));
-  while (!('winner' in stateAfterAutoPlay)) {
-    if (stateAfterAutoPlay.lastPieceMoved === playerVal) {
+  runInteractiveMode(stateAfterAutoPlay, playerVal, computerVal, numConsecutive);
+}
+
+const useControlledGameSettings = function () {
+  const userInput1 = readlineSync.question(
+    'Enter the number of rows, columns, and consecutive "pieces" for win\n' +
+    '(all separated by commas... for example: 6,7,4)\n' +
+    '>'
+  );
+  const [numRowsStr, numColumnsStr, numConsecutiveStr] = userInput1 === '' ? ['6', '7', '4'] : userInput1.split(',');
+  const [numRows, numColumns, numConsecutive] = [parseInt(numRowsStr), parseInt(numColumnsStr), parseInt(numConsecutiveStr)];
+  console.log(`Using row, col and consecutive: ${numRows} ${numColumns} ${numConsecutive}`);
+
+  const userInput2 = readlineSync.question(
+    'Enter two characters that represent the player and computer\n' +
+    '(separated by a comma... for example: P,C)\n' +
+    '>'
+  );
+  const [playerVal, computerVal] = userInput2 === '' ? ['😎', '💻'] : userInput2.split(',');
+  console.log(`Using player and computer characters: ${playerVal} ${computerVal}`);
+
+  const userInput3 = readlineSync.question(' Who goes first, (P)layer or (C)omputer?\n>');
+  const [p1, p2] = userInput3 === 'C' ? [computerVal, playerVal] : [playerVal, computerVal];
+  console.log(`${p1} goes first`);
+
+  const initialState = {
+    board: c.generateBoard(numRows, numColumns),
+    pieces: [p1, p2],
+    lastPieceMoved: p2
+  }
+  readlineSync.question('Press <ENTER> to start game');
+  runInteractiveMode(initialState, playerVal, computerVal, numConsecutive);
+}
+
+const runInteractiveMode = function (state, playerVal, computerVal, numConsecutive) {
+  console.log(c.boardToString(state.board));
+
+  let letter = null;
+  while (!('winner' in state) && state.board.data.some(x => (x == null))) {
+    if (state.lastPieceMoved === playerVal) {
+      readlineSync.question('Press <ENTER> to see computer move');
       clear();
       let nullOrRowCal = null;
       while (nullOrRowCal === null) {
-        let randomOffset = Math.floor(Math.random() * Math.floor(numColumns));
+        let randomOffset = Math.floor(Math.random() * Math.floor(state.board.cols));
         letter = String.fromCharCode(65 + randomOffset);
-        nullOrRowCal = c.getEmptyRowCol(stateAfterAutoPlay.board, letter);
+        nullOrRowCal = c.getEmptyRowCol(state.board, letter);
       }
-      stateAfterAutoPlay = c.autoplay(stateAfterAutoPlay.board, `${computerVal}${playerVal}${letter}`, numConsecutive);
-      console.log(c.boardToString(stateAfterAutoPlay.board));
+      console.log(`...dropping in column ${letter}`);
+      state = c.autoplay(state.board, `${computerVal}${playerVal}${letter}`, numConsecutive);
+      console.log(c.boardToString(state.board));
     } else {
       let nullOrRowCal = null;
       while (nullOrRowCal === null) {
         letter = readlineSync.question('Choose a column letter to drop your piece in\n>');
-        nullOrRowCal = c.getEmptyRowCol(stateAfterAutoPlay.board, letter);
+        nullOrRowCal = c.getEmptyRowCol(state.board, letter);
         if (nullOrRowCal === null) {
           console.log('Oops, that is not a valid move, try again!');
         }
       }
       clear();
       console.log(`...dropping in column ${letter}`);
-      stateAfterAutoPlay = c.autoplay(stateAfterAutoPlay.board, `${playerVal}${computerVal}${letter}`, numConsecutive);
-      console.log(c.boardToString(stateAfterAutoPlay.board));
+      state = c.autoplay(state.board, `${playerVal}${computerVal}${letter}`, numConsecutive);
+      console.log(c.boardToString(state.board));
     }
 
   }
-  console.log(`Winner is ${stateAfterAutoPlay.winner}`);
-}
-
-const useControlledGameSettingsAndRunInteractiveGame = function () {
-  const userInput1 = readlineSync.question(
-    'Enter the number of rows, columns, and consecutive "pieces" for win\n' +
-    '(all separated by commas... for example: 6,7,4)\n' +
-    '>'
-  );
-  const [numRowsStr, numColumnsStr, numConsecutiveStr] = userInput1 === '' ? ['6', '7', '4'] : userInput1.split(',')
-  const [numRows, numColumns, numConsecutive] = [parseInt(numRowsStr), parseInt(numColumnsStr), parseInt(numConsecutiveStr)];
-  console.log(`Using row, col and consecutive: ${numRows} ${numColumns} ${numConsecutive}`)
-
-  const userInput2 = readlineSync.question(
-    'Enter two characters that represent the player and computer\n' +
-    '(separated by a comma… for example: P,C)\n' +
-    '>'
-  );
-  const [playerVal, computerVal] = userInput2 === '' ? ['😎', '💻'] : userInput2.split(',')
-  console.log(`Using player and computer characters: ${playerVal} ${computerVal}`)
-
-  const userInput3 = readlineSync.question(' Who goes first, (P)layer or (C)omputer?\n>');
-  const [p1, p2] = userInput3 === 'C' ? [computerVal, playerVal] : [playerVal, computerVal];
-  console.log(`${p1} goes first`)
-
-  readlineSync.question('Press <ENTER> to start game');
-
-
+  if ('winner' in state) {
+    console.log(`Winner is ${state.winner}`);
+  } else {
+    console.log('No Winner. So sad 😭');
+  }
 }
 
 if (process.argv.length > 2) {
   useScriptedMoves();
 } else {
-  useControlledGameSettingsAndRunInteractiveGame();
+  useControlledGameSettings();
 }
